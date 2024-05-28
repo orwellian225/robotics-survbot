935e1aa8029e58b7e72bac3163def088a3431509
orwellian <orwellian225@gmail.com>
2024-05-28 19:14:05 +0200

    modify: PID    add: PID Controller    add: Scan state

---
+++ src/PIDController.py
@@ -0,0 +1,78 @@
+import math as m
+import rospy as rp
+from Vec2 import Vec2
+
+from geometry_msgs.msg import Twist
+
+class PIDController:
+    def __init__(self, velocity_topic,
+        position_Kp, position_Ki, position_Kd,
+        yaw_Kp, yaw_Ki, yaw_Kd 
+    ):
+        self.reset_pid()
+        self.velocity_pub = rp.Publisher(velocity_topic, Twist, queue_size=1)
+
+        self.yaw_Kp = yaw_Kp
+        self.yaw_Ki = yaw_Ki
+        self.yaw_Kd = yaw_Kd
+
+        self.position_Kp = position_Kp
+        self.position_Ki = position_Ki
+        self.position_Kd = position_Kd
+
+    def reset_pid(self):
+        self.position_sum_error = 0.
+        self.position_diff_error = 0.
+
+        self.yaw_sum_error = 0.
+        self.yaw_diff_error = 0.
+
+    def apply_pid(self, current_position, current_yaw, target_position):
+        """
+            Apply PID Movement to SurvBot
+            
+            Return Values:
+                -1 => Error has occurred
+                0 => Not reached target
+                1 => Reached target
+        """
+
+        pos_error = current_position.distance_to(target_position)
+        self.pos_sum_error += pos_error
+        pos_diff_error = pos_error - self.pos_prev_error
+        self.pos_prev_error = pos_error
+
+        target_direction = target_position - current_position
+        bot_direction = Vec2( m.cos(current_yaw), m.sin(current_yaw) )
+        yaw_error = target_direction.angle_to(bot_direction)
+        self.yaw_sum_error += yaw_error
+        yaw_diff_error = yaw_error - self.yaw_prev_error
+        self.yaw_prev_error = yaw_error
+
+        linear_vel = self.position_Kp * pos_error + self.position_Ki * self.pos_sum_error + self.position_Kd * pos_diff_error
+        angular_vel = self.yaw_Kp * yaw_error + self.yaw_Ki * self.yaw_sum_error + self.yawk_Kd * yaw_diff_error
+
+        msg = Twist()
+        if angular_vel < 1e-2:
+            msg.linear.x = linear_vel
+        msg.angular.z = angular_vel
+
+        rp.loginfo("PID Target (x,y): (%f,%f)", self.nav_target.x, self.nav_target.y)
+        rp.loginfo("PID Moving to (x,y): (%f,%f)", target_position.x, target_position.y)
+        rp.loginfo("PID Distance Error: %f", pos_error)
+        rp.loginfo("PID Turn Error: %f", yaw_error)
+        rp.loginfo("PID forward Velocity : %f", linear_vel)
+        rp.loginfo("PID Turn Velocity: %f", angular_vel)
+
+        # If SurvBot hasn't moved enough, then exit with an error
+        if pos_diff_error < 1e-3 and yaw_diff_error < 1e-3:
+            return -1
+
+        if pos_error < 0.3:
+            reset = Twist()
+            self.velocity_pub.publish(reset)
+            return 1
+
+        self.velocity_pub.publish(msg)
+
+        return 0 
--- src/SurvBot.py
+++ src/SurvBot.py
@@ -1,10 +1,10 @@
 import numpy as np
 import rospy as rp
 import math as m
-import random
 
 from Vec2 import Vec2
 from Graph import Graph
+from PIDController import PIDController
 
 from std_msgs.msg import String, Empty
 from geometry_msgs.msg import Vector3, Twist
@@ -14,14 +14,13 @@ from gazebo_msgs.srv import GetModelState
     Survillance Bot
 
     States
+        * SCAN -> Rotate in place for a specified number of times
         * NAVIGATE -> Move towards the specified goal
         * PATROL -> Move along a cycle in the graph
         * PATHFIND -> Update the move queue to the Navigation Targat
         * IDLE -> Do Nothing
 """
-all_states = ["IDLE", "NAVIGATE", "PATHFIND", "PATROL"]
-num_vertices = 1000
-num_neighbours = 20
+all_states = ["IDLE", "PATHFIND", "NAVIGATE", "SCAN",  "PATROL"]
 class SurvBot:
     def __init__(self):
         self.state = "IDLE"
@@ -29,44 +28,19 @@ class SurvBot:
         self.init_ros_subscribers()
         self.init_ros_publishers()
 
+        self.pid = PIDController(
+            '/mobile_base/commands/velocity',
+            0.1, 0., 0., 1., 0., 0.
+        )
 
         self.nav_target = Vec2(0,0)
+        self.rotations = 1.
+
         self.position = Vec2(0,0)
         self.yaw = 0
 
-        self.pos_sum_error = 0
-        self.pos_prev_error = 0
-        self.yaw_sum_error = 0
-        self.yaw_prev_error = 0
-
         self.move_queue = []
-
-        # generate_graph
-        self.graph = Graph("./maps/map.txt", "./maps/map.yaml")
-        # print([str(x) for x in self.graph.vertices])
-        # print(self.graph.adjacencies)
-
-        world_min = Vec2(0,0)
-        world_max = Vec2(self.graph.map_width,self.graph.map_height)
-        for i in range(num_vertices):
-            if i % 50 == 0:
-                rp.loginfo("Adding vertex %d", i)
-            x = random.randint(int(world_min.x), int(world_max.x - 1))
-            y = random.randint(int(world_min.y), int(world_max.y - 1))
-
-            new_vertex = self.graph.pixel_to_world(Vec2(x, y))
-            self.graph.insert_vertex(new_vertex, num_neighbours)
-
-        print([str(x) for x in self.graph.vertices])
-        print(self.graph.adjacencies)
-
-    def init_ros_subscribers(self):
-        self.state_sub = rp.Subscriber('/survbot/state', String, self.change_state) # Change state
-        self.navgoal_sub = rp.Subscriber('/survbot/state/navigate/goal', Vector3, self.update_goal) # Change Navigate goal
-
-    def init_ros_publishers(self):
-        self.velocity_pub = rp.Publisher('/mobile_base/commands/velocity', Twist, queue_size=1)
-        self.notify_pub = rp.Publisher('/survbot/notifications', String, queue_size=1)
+        self.state_queue = []
 
     def loop(self):
         rp_rate = rp.Rate(10)
@@ -77,12 +51,13 @@ class SurvBot:
 
     def update(self):
         coordinates, rotation = self.get_world_state()
-        if self.state != "IDLE":
-            rp.loginfo("\n")
-            rp.loginfo("BOT Status: %s", self.state)
-            rp.loginfo("BOT Move Queue: %s", len(self.move_queue))
-            rp.loginfo("BOT Position (x,y,z): (%f, %f, %f)", coordinates.x, coordinates.y, coordinates.z)
-            rp.loginfo("BOT Rotation (w,x,y,z): (%f, %f, %f, %f)", rotation.w, rotation.x, rotation.y, rotation.z)
+        # if self.state != "IDLE":
+        rp.loginfo("\n")
+        rp.loginfo("BOT Status: %s", self.state)
+        rp.loginfo("BOT Move Queue length: %d", len(self.move_queue))
+        rp.loginfo("BOT Order Queue length: %d", len(self.move_queue))
+        rp.loginfo("BOT Position (x,y,z): (%f, %f, %f)", coordinates.x, coordinates.y, coordinates.z)
+        rp.loginfo("BOT Rotation (w,x,y,z): (%f, %f, %f, %f)", rotation.w, rotation.x, rotation.y, rotation.z)
 
         self.position = Vec2(coordinates.x, coordinates.y)
         self.yaw = m.atan2( 2 * ( rotation.w * rotation.z + rotation.x * rotation.y ), 1 - 2 * ( rotation.y**2 + rotation.z**2 ) )
@@ -93,71 +68,50 @@ class SurvBot:
             self.state_pathfind()
         elif self.state == "NAVIGATE":
             self.state_navigate()
+        elif self.state == "SCAN":
+            self.state_scan()
         elif self.state == "PATROL":
             self.state_pathfind()
+
+    def change_state(self, new_state):
+        new_state = new_state.upper()
+        if new_state in all_states:
+            rp.loginfo("Changing state: %s -> %s", self.state, new_state)
+
+            self.pid.reset_pid()
+            self.state = new_state 
+        else:
+            rp.logwarn("Invalid state requested: %s does not exist", new_state)
 
     def state_idle(self):
         pass
 
     def state_pathfind(self):
-        
-        start_result = self.graph.insert_vertex(self.position, num_neighbours)
-        end_result = self.graph.insert_vertex(self.nav_target, num_neighbours)
-
-        if not start_result or not end_result:
-            rp.logerr("Failed to insert vertex")
-            self.internal_change_state("IDLE")
-            return
-
-        self.move_queue = self.graph.a_star(len(self.graph.vertices) - 2, len(self.graph.vertices) - 1)
-
-        self.graph.remove_vertex(self.position)
-        self.graph.remove_vertex(self.nav_target)
-        
-        self.internal_change_state("NAVIGATE")
+        pass
 
     def state_navigate(self):
         if len(self.move_queue) == 0:
             rp.loginfo("Move Queue is Empty")
-            self.internal_change_state("IDLE")
+            self.change_state("IDLE")
             return
 
-        reached_target = self.move_to(self.position, self.yaw, self.move_queue[0])
+        pid_status = self.move_to(self.position, self.yaw, self.move_queue[0])
 
-        if reached_target:
+        if pid_status == 1:
             self.move_queue.pop(0)
+        elif pid_status == -1:
+            rp.logwarn("SurvBot PID failed to move enough")
+            self.change_state("IDLE")
+
+    def state_scan(self):
+        pass
 
     def state_patrol(self):
         pass
-        # next_target = self.move_queue[0]
-        # reached_target = self.move_to(self.position, self.yaw, next_target)
 
-        # if reached_target:
-        #     self.move_queue.pop(0)
-        #     self.move_queue.append(next_target)
-
-    def internal_change_state(self, new_state):
-        new_state = new_state.upper()
-        if new_state in all_states:
-            rp.loginfo("Changing state: %s -> %s", self.state, new_state)
-
-            self.pos_sum_error = 0
-            self.pos_prev_error = 0
-            self.yaw_sum_error = 0
-            self.yaw_prev_error = 0
-
-            notification = String()
-            notification.data = "state_change {0} -> {1}".format(self.state, new_state)
-            self.notify_pub.publish(notification)
-
-            self.state = new_state 
-        else:
-            rp.logwarn("Invalid state requested: %s does not exist", new_state)
-
-
-    def change_state(self, data):
+    def update_state(self, data):
         new_state = str(data.data)
-        self.internal_change_state(new_state)
+        self.change_state(new_state)
 
     def update_goal(self, data):
         coord_x = data.x
@@ -165,8 +119,8 @@ class SurvBot:
         self.nav_target = Vec2(coord_x, coord_y)
         rp.loginfo("Update Navigate Goal (x,y): (%f, %f)", coord_x, coord_y)
 
-    def refresh_patrol_path(self):
-        rp.loginfo("Refresh patrol path")
+    def update_rotations(self, data):
+        pass
 
     def get_world_state(self):
         rp.wait_for_service("/gazebo/get_model_state")
@@ -181,55 +135,19 @@ class SurvBot:
             rp.logerr("Service call failed: %s", e)
             return None
 
-    def move_to(self, current_position, current_yaw, target_position):
-        Kp_pos = 0.1
-        Ki_pos = 0
-        Kd_pos = 0
+    def init_ros_subscribers(self):
+        self.state_sub = rp.Subscriber('/survbot/state', String, self.update_state) # Change state
+        self.navgoal_sub = rp.Subscriber('/survbot/navigate/goal', Vector3, self.update_goal) # Change Navigate goal
+        self.rotate_sub = rp.Subscriber('/survbot/scan/rotations', Vector3, self.update_goal) # Change Navigate goal
 
-        Kp_yaw = 1
-        Ki_yaw = 0
-        Kd_yaw = 0
-
-        pos_error = current_position.distance_to(target_position)
-        self.pos_sum_error += pos_error
-        pos_diff_error = pos_error - self.pos_prev_error
-        self.pos_prev_error = pos_error
-
-        target_direction = target_position - current_position
-        bot_direction = Vec2( m.cos(current_yaw), m.sin(current_yaw) )
-        yaw_error = target_direction.angle_to(bot_direction)
-        self.yaw_sum_error += yaw_error
-        yaw_diff_error = yaw_error - self.yaw_prev_error
-        self.yaw_prev_error = yaw_error
-
-        linear_vel = Kp_pos * pos_error + Ki_pos * self.pos_sum_error + Kd_pos * pos_diff_error
-        angular_vel = Kp_yaw * yaw_error + Ki_yaw * self.yaw_sum_error + Kd_yaw * yaw_diff_error
-
-        msg = Twist()
-        if angular_vel < 1e-2:
-            msg.linear.x = linear_vel
-        msg.angular.z = angular_vel
-
-        rp.loginfo("Target (x,y): (%f,%f)", self.nav_target.x, self.nav_target.y)
-        rp.loginfo("Moving to (x,y): (%f,%f)", target_position.x, target_position.y)
-        rp.loginfo("Distance Error: %f", pos_error)
-        rp.loginfo("Turn Error: %f", yaw_error)
-        rp.loginfo("forward Vel : %f", linear_vel)
-        rp.loginfo("turn Vel : %f", angular_vel)
-
-        if pos_error < 0.3:
-            reset = Twist()
-            self.velocity_pub.publish(reset)
-            return True
-
-        self.velocity_pub.publish(msg)
-
-        return False
+    def init_ros_publishers(self):
+        # self.notify_pub = rp.Publisher('/survbot/notifications', String, queue_size=1)
+        pass
 
 
 def main():
     """
-        Start the surveillance bot when this file is run
+        Start SurvBot when this file is run
     """
     try:
         surv_bot = SurvBot()
